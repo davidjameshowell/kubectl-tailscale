2a798f1b4c4c3b425b1d33d43222f11e97e41394
David Howell <david@davidjameshowell.com>
2021-07-31 16:14:15 -0700

    Add Tailscale to Kubectl action

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,8 @@ set -e
 version="$1"
 config="$2"
 command="$3"
+tailscale_version="$4"
+tailscale_key="$5"
 
 if [ "$version" = "latest" ]; then
   version=$(curl -Ls https://dl.k8s.io/release/stable.txt)
@@ -19,5 +21,23 @@ mv kubectl /usr/local/bin
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 echo "$config" | base64 -d > /tmp/config
 export KUBECONFIG=/tmp/config
+
+# Start Tailscale config
+MINOR=$(echo $tailscale_version | awk -F '.' {'print $2'})
+if [ $((MINOR % 2)) -eq 0 ]; then
+  URL="https://pkgs.tailscale.com/stable/tailscale_${tailscale_version}_amd64.tgz"
+else
+  URL="https://pkgs.tailscale.com/unstable/tailscale_${tailscale_version}_amd64.tgz"
+fi
+curl $URL -o tailscale.tgz
+tar -C ~ -xzf tailscale.tgz
+rm tailscale.tgz
+mv "$HOME/tailscale_${tailscale_version}_amd64" ~/.tailscale
+echo "PATH=$PATH:$HOME/.tailscale" >> $GITHUB_ENV
+
+# Start Tailscale
+sudo $(which tailscaled) 2>~/.tailscale/tailscaled.log &
+sleep 3
+sudo $(which tailscale) up --authkey $tailscale_key --hostname "github-$(cat /etc/hostname)"
 
 sh -c "kubectl $command"
